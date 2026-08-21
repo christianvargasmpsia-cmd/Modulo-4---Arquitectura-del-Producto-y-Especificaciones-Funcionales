d9c091de917ce64866bec929d5c6f9ce98259f53
ElChris496 <christianvs000@gmail.com>
2026-08-21 18:28:50 -0400

    arreglo rag deshabilitado

--- backend/umss-market-api/src/test/java/bo/umss/market/umss_market_api/UmssMarketApiApplicationTests.java
+++ backend/umss-market-api/src/test/java/bo/umss/market/umss_market_api/UmssMarketApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled("Test de integración — requiere PostgreSQL")
+//@Disabled("Test de integración — requiere PostgreSQL")
 @SpringBootTest
 class UmssMarketApiApplicationTests {
 
